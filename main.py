868f5f51ed468a20d55823726bf13a37cbce6858
gutsy47 <bestcom5055@gmail.com>
2022-10-30 03:37:04 +0300

    Feat: Add "click-me" arrow overlay
    New feature "add arrow" provided. Arrow should be turned to the right, scrip will set it close to music button and rotate it 45 degrees clockwise

--- main.py
+++ main.py
@@ -43,6 +43,7 @@ def main():
     in_files = get_folder_files(config["path"]["input"])
     images = get_list_of_images(config["path"]["image"])
     sounds = get_folder_files(config["path"]["audio"])
+    arrows = get_folder_files(config["path"]["arrow"])
 
     crop_start, crop_end = int(config["crop"]["start"]), int(config["crop"]["end"])
     fadein_duration = int(config["fadein"]["duration"])
@@ -65,7 +66,8 @@ def main():
         f" INPUT | {config['path']['input']}\n\t"
         f"OUTPUT | {config['path']['output']}\n\t"
         f"IMAGES | {config['path']['image']}\n\t"
-        f"AUDIOS | {config['path']['audio']}\n"
+        f"AUDIOS | {config['path']['audio']}\n\t"
+        f"ARROWS | {config['path']['arrow']}\n"
         "\nSettings:\n\t"
         f"  CROP | {crop_start}:{crop_end}\n\t"
         f"FADEIN | {fadein_duration}\n\t"
@@ -80,29 +82,43 @@ def main():
     timer_start = default_timer()
     for index, filename in enumerate(get_folder_files(config["path"]["input"])):
 
-        # Just logs
+        # Log
         print(Back.YELLOW + "Processing:" + Style.RESET_ALL, filename)
 
         # Proceed the video
-        clip: VideoFileClip = VideoFileClip(filename, audio=False)  # Get the clip
+        clip: VideoFileClip = VideoFileClip(filename, audio=False)
         if crop_end > clip.duration:
             crop_end = clip.duration - crop_start
-        clip = clip.subclip(crop_start, crop_end)  # Crop
-        clip = vfx.fadein(clip, duration=fadein_duration)  # Fade in
-        clip = vfx.mirror_x(clip)  # Vertical flip
+        clip = clip.subclip(crop_start, crop_end)
+        clip = vfx.fadein(clip, duration=fadein_duration)
+        clip = vfx.mirror_x(clip)
 
-        # Add noise
+        # Add arrow
+        arrow: VideoFileClip = VideoFileClip(choice(arrows), has_mask=True)
+        arrow = arrow.set_duration(clip.duration)
+        arrow = arrow.resize(height=75)
+        position = (
+            0.5 + 0.5**2 + 0.5**4 - arrow.size[0]/clip.size[0],
+            0.5 + 0.5**2
+        )
+        arrow = arrow.set_position(position, relative=True)
+        arrow = arrow.rotate(-45)
+
+        # Add sound
         audio: AudioFileClip = AudioFileClip(choice(sounds))
-        # if crop_end > audio.duration:
-        #     crop_end = audio.duration - crop_start
-        audio = audio.subclip(crop_start, crop_end)  # Get audio and crop
+        if crop_end > audio.duration:
+            crop_end = audio.duration - crop_start
+        audio = audio.subclip(crop_start, crop_end)
         clip = clip.set_audio(audio)
 
-        # Compose clip with image
-        image = choice(images).set_duration(image_duration).resize(image_size)
-        clip: CompositeVideoClip = CompositeVideoClip(
-            [clip, image.set_position(("center", 0.68), relative=True).set_start(clip.duration - 5)]
-        )
+        # Add image
+        image: ImageClip = choice(images)
+        image = image.set_duration(image_duration)
+        image = image.resize(image_size)
+        image = image.set_position(("center", 0.68), relative=True).set_start(clip.duration - 5)
+
+        # Compose clip
+        clip: CompositeVideoClip = CompositeVideoClip([clip, image, arrow])
 
         # Save the clip
         outfile = get_out_filename(config["path"]["output"])
